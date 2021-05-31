1cfdc8d4ff386e02700bd7c706e6928328b38f88
rustamli <mc2run@gmail.com>
2021-05-31 22:41:15 +0100

    latest ui

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const fastify = require('fastify')();
 const path = require('path');
 const gen = require('./epub-generator');
 
-// fastify.register(require('fastify-static'), {
-//   root: path.join(__dirname, 'public')
-// });
-
-fastify.get('/', function(request, reply) {
-  gen.run();
-  reply.send({ status: 'done' });
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public')
 });
 
-fastify.get('/generate', function(request, reply) {
-  gen.run();
-  reply.send({ status: 'done' });
+fastify.get('/generate', async function(request, reply) {
+  await gen.run();
+  reply.redirect('/the-road-to-mecca.epub');
 });
 
 fastify.listen(3000, function(err, address) {
