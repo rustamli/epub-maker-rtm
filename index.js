b3dcd5f9d28c1b1ba4d600cfa953cf739a9b3210
rustamli <mc2run@gmail.com>
2021-05-31 22:18:31 +0100

    added fastify server

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const fastify = require('fastify')();
 const gen = require('./epub-generator');
 
-gen.run();
+fastify.get('/generate', function(request, reply) {
+  gen.run();
+  reply.send({ status: 'done' });
+});
+
+fastify.get('/', function(request, reply) {
+  reply.send('Hello');
+});
+
+fastify.listen(3000, function(err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
