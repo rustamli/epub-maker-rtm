1f4fc88357ddf77ac745cf1c0a210e11e3ab6369
rustamli <mc2run@gmail.com>
2021-05-31 23:45:15 +0100

    port 8080

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ fastify.get('/generate', async function(request, reply) {
   gen.run(reply);
 });
 
-fastify.listen(80, function(err, address) {
+fastify.listen(8080, function(err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
