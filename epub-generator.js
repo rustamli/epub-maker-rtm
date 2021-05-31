ecc21cc14d5fd36f5396760ef3d292df953a5db1
rustamli <mc2run@gmail.com>
2021-05-31 23:07:14 +0100

    added about translation

--- epub-generator.js
+++ epub-generator.js
@@ -2,7 +2,8 @@ const cheerio = require('cheerio');
 const Epub = require('epub-gen');
 const fetch = require('node-fetch');
 
-const chapters = `front-matter
+const chapters = `about-translation
+front-matter
 glossary
 story-of-a-story
 i-thirst
@@ -19,7 +20,9 @@ xi-jihad
 xii-end-of-the-road`.split('\n');
 
 function getUrl(chapter) {
-  return `https://raw.githubusercontent.com/inuritdino/TheRoadToMecca/master/src/${chapter}/index.html`;
+  return chapter === 'about-translation'
+    ? 'https://raw.githubusercontent.com/inuritdino/TheRoadToMecca/master/about-translation/index.html'
+    : `https://raw.githubusercontent.com/inuritdino/TheRoadToMecca/master/src/${chapter}/index.html`;
 }
 
 async function downloadChapter(chapter) {
@@ -42,14 +45,28 @@ async function processChapter(chapter) {
     $('h2').remove();
   }
 
+  let data = $('#content-1')
+    .html()
+    .split('\n')
+    .join('');
+
+  if (chapter === 'about-translation') {
+    data = data
+      .replace(
+        'Перевод выполнен участниками проекта',
+        'Перевод выполнен участниками проекта '
+      )
+      .replace('Главный переводчик:', 'Главный переводчик: ')
+      .replace('(Muhammad Asad)', ' (Muhammad Asad)')
+      .replace('Dar al-Andalus 1980(', 'Dar al-Andalus 1980 (');
+  }
+
   return {
     title: $('title')
       .html()
-      .split('Дорога в Мекку. ')[1],
-    data: $('#content-1')
-      .html()
-      .split('\n')
-      .join('')
+      .split('Дорога в Мекку. ')[1]
+      .replace('переводе.', 'переводе'),
+    data: data
   };
 }
 
